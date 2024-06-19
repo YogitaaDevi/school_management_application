b222f539ac40763f7debba506af10599b4ff5238
YogitaaDevi <yogitaadevi.ravishankar@ideas2it.com>
2024-06-19 19:43:23 +0530

    maven project

--- com/i2i/sma/exception/SchoolManagementException.java
+++
@@ -1,9 +0,0 @@
-package com.i2i.sma.exception;
-public class SchoolManagementException extends Exception {
-    public SchoolManagementException(String message) {
-        super(message);
-   }
-    public SchoolManagementException(String message, Throwable e) {
-        super(message, e);
-   }
-}
